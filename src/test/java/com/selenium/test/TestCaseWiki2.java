1828015938967c11c68d8b8aa7ee9c9a85b2e773
Facundo Luna <89422@sistemas.frc.utn.edu.ar>
2023-06-02 20:12:52 -0300

    src vuelve a estar subida

---
+++ src/test/java/com/selenium/test/TestCaseWiki2.java
@@ -0,0 +1,52 @@
+package com.selenium.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.selenium.page.*;
+import com.selenium.driver.*;
+import com.selenium.MetodosUtiles.*;
+
+public class TestCaseWiki2 {
+	WebDriver driver;
+
+	@DataProvider(name = "datos")
+	public Object[][] createData() {
+		return new Object[][] { 
+			{ "Selenium", "Selenium" }, 
+			{ "TDD", "Desarrollo guiado por pruebas" },
+			{"JAVA","Java (lenguaje de programación)"},
+			{ "DATA DRIVEN TESTING", "Data-driven testing" } 
+		};
+	}
+	//TODO "Crear un metodo para obtener datos externos."
+	@BeforeMethod
+	public void abrirBrowser(ITestContext context) {
+		
+		driver = DriverFactory.LevantarBrowser(driver, context);
+	}
+	@Test(dataProvider = "datos", description="Validar y Verificar que Wikipedia cotiene el campo")
+	public void validarCajaTextoNuevo(String varBuscar, String resultado) throws Exception{
+		WikiHomePage wikihomepage = PageFactory.initElements(driver, WikiHomePage.class);
+		wikihomepage.IngresarDatoCajaBusqueda(varBuscar);
+		Utiles.reportes("Validar que el titulo sea "+resultado);
+		WikiResultadosPage wikiRdopage= PageFactory.initElements(driver, WikiResultadosPage.class);
+		
+		Utiles.reportes("Validar que el titulo sea "+ resultado);
+		Assert.assertTrue((wikiRdopage.ObtenerTitulo().contains(resultado)), "El valor "+resultado+" no se encuentra en el titulo");
+	}
+	
+	@AfterMethod
+	public void CerrarBrowser() {
+		Utiles.reportes("Cerrar Browser");
+		DriverFactory.FinalizarBrowser(driver);
+	}
+	
+	
+}
---
+++ src/test/java/com/selenium/test/TestDespegar.java
@@ -0,0 +1,90 @@
+package com.selenium.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.ITestContext;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import com.selenium.MetodosUtiles.*;
+import com.selenium.driver.DriverFactory;
+import com.selenium.page.DespegarHomePage;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+import java.time.LocalDate;
+
+public class TestDespegar {
+	WebDriver driver;
+	WebElement btnEscLogin;
+	WebElement btnNoLogin;
+	WebElement btnCookies;
+	WebElement txtDestino;
+	WebElement txtOrigen;
+	LocalDate fechaActual;
+	int diaActual;
+	int mesActual;
+	int anioActual;
+	String fecha;
+	String xpathIda;
+	String xpathVuelta;
+	WebElement fechaIda;
+	WebElement selectFechaIda;
+	WebElement fechaVuelta;
+	WebElement selectFechaVuelta;
+	WebElement btnAplicarPeriodo;
+	WebElement habitaciones;
+	WebElement btnBuscar;
+	WebDriverWait wait;
+
+	@BeforeMethod()
+	public void abrirBrowser(ITestContext context) throws Exception {
+		driver = DriverFactory.LevantarBrowser(driver, context);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+	}
+
+	@AfterMethod()
+	public void cerrarBrowser() throws Exception {
+		driver.quit();// a diferencia del diver.close(); esa cierra toda la ventana sin importar la
+						// cantidad de pensatañas creadas
+		Utiles.reportes("Cerramos el Browser correctamente.");
+		Utiles.reportes("Fin CP.");
+	}
+
+	@DataProvider(name = "datos")
+	public Object[][] createData() {
+		return new Object[][] {
+				// Destino, Fecha ida(cant dias a partir de hoy), Fecha Vuelta(cant dias)
+				{ "Buenos Aires, Argentina", "San Carlos de Bariloche, Rio Negro, Argentina", 2, 2 },
+				{ "Buenos Aires, Argentina", "Villa Carlos Paz, Córdoba, Argentina", 2, 2 },
+				{ "Buenos Aires, Argentina", "Villa La Angostura, Neuquén, Argentina", 2, 2 } };
+	}
+
+	@Test(dataProvider = "datos", description = "Buscar paquetes en Despegar.com")
+	public void validarDespegar(String Origen, String destino, int salida, int duracion) throws Exception {
+
+		DespegarHomePage despegarHomePage = PageFactory.initElements(driver, DespegarHomePage.class);
+		despegarHomePage.cerrarLogin();
+		despegarHomePage.aceptarCookies();
+
+		// Ingresar Origen
+		despegarHomePage.ingresarSalida(Origen);
+
+		// Ingresar Destino
+		despegarHomePage.ingresarDestino(destino);
+
+		// Seleccion dias
+		despegarHomePage.seleccionarFechaIda(salida, duracion);
+
+		// Seleccionamos la fecha de vuelta
+		despegarHomePage.seleccionFechaVuelta(salida, duracion);
+
+		// Buscamos los Paquetes
+		despegarHomePage.realizarBusqueda();
+
+		// Verificamos si la url es de la busqueda
+		despegarHomePage.verificarUrlBusqueda(wait);
+
+	}
+}
